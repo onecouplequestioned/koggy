460f56436f7724f1b0ab77fb93e2d845cb98b25c
Hydrangea <onecouplequestioned.onecouplequestioned@gmail.com>
2016-12-14 16:08:45 -0500

    a test

---
+++ test.py
@@ -0,0 +1,10 @@
+
+
+def stringify_bool(n):
+	if n + 1 == 1:
+		return "true"
+	else:
+			return "false"
+
+
+#18<flavius> def stringify_bool(value): if value: return "true" else: return "false"
