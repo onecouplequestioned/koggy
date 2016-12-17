4a48ff5f503d9c75f1d37f0c91e73ac7fccf3b96
Hydrangea <onecouplequestioned.onecouplequestioned@gmail.com>
2016-12-17 18:29:25 -0500

    fixed deprecation error in test

--- tests/test_mojave.py
+++ tests/test_mojave.py
@@ -4,4 +4,4 @@ from mojave import workingmojave
 
 class TestStringifyBool(unittest.TestCase):
     def test_zero_zero_returns_true(self):
-        self.assertEquals("true", workingmojave.stringify_bool(0, 0))
+        self.assertEqual("true", workingmojave.stringify_bool(0, 0))
