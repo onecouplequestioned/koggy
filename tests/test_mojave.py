a39512fae887623e338639c29f7f43d86b01be53
Hydrangea <onecouplequestioned.onecouplequestioned@gmail.com>
2016-12-17 20:13:05 -0500

    added invalid and valid tests for det2x2 function for SUT within test_mojave.py

--- tests/test_mojave.py
+++ tests/test_mojave.py
@@ -5,3 +5,15 @@ from mojave import workingmojave
 class TestStringifyBool(unittest.TestCase):
     def test_zero_zero_returns_true(self):
         self.assertEqual("true", workingmojave.stringify_bool(0, 0))
+
+    def testing_det2x2_valid(self):
+    	self.assertEqual(-2, workingmojave.det2x2([3, 2, 4, 2])
+		self.assertEqual(0, workingmojave.det2x2([0, 0, 0, 0])
+		self.assertEqual(0, workingmojave.det2x2([-1, -1, -1, -1])
+
+
+	def testing_det2x2_invalid(self):
+        self.assertEqual(None, workingmojave.det2x2(["carrot", "waffles", "donuts", "parrots"])
+		self.assertEqual(None, workingmojave.det2x2([2, 5, 2])
+		self.assertEqual(None, workingmojave.det2x2([2, 5,])
+		self.assertEqual(None, workingmojave.det2x2([2, ])
