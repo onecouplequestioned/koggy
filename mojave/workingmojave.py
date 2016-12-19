05db0bfeac012a7366c143fa4a9092ca8007984e
Hydrangea <onecouplequestioned.onecouplequestioned@gmail.com>
2016-12-18 21:23:41 -0500

    added det2x2 tests. test_mojave.py is currently broken.

--- mojave/workingmojave.py
+++ mojave/workingmojave.py
@@ -3,3 +3,12 @@ def stringify_bool(n,x):
 		return "true"
 	else:
 		return "false"
+
+
+
+def det2x2(a, b, c, d):
+	mat_list = [a, b, c, d]
+	e = ((a * b) - (c * d))
+	print(e)
+
+#3×6 − 8×4 = 18 − 32 = −14
