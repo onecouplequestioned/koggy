6ec25813afd24ecf7576f0fd6e5bd93ac0ccfc77
Hydrangea <onecouplequestioned.onecouplequestioned@gmail.com>
2016-12-19 08:29:27 -0500

    modified the det2x2 test considerably, added a det3x3 test that works similarly to mojave.py . modified the tests to run the added and modified code from mojave.py to test_mojave.py. the code that is supposed to work does, the code that is not supposed to work does not

--- mojave/workingmojave.py
+++ mojave/workingmojave.py
@@ -1,3 +1,8 @@
+import math
+import numpy as np
+
+
+
 def stringify_bool(n,x):
 	if n + x == 0:
 		return "true"
@@ -6,9 +11,32 @@ def stringify_bool(n,x):
 
 
 
-def det2x2(a, b, c, d):
-	mat_list = [a, b, c, d]
-	e = ((a * b) - (c * d))
-	print(e)
+def det2x2(det2_list):
+    a,b,c,d = det2_list[0], det2_list[1], det2_list[2], det2_list[3]
+
+    e = [a,b]
+    f = [c,d]
+    g = ((a * b)-(c*d))
+
+    print(e)
+    print(f)
+    return g
+
+
+def det3x3(det3_list):
+    a,b,c,d,e,f,g,h,i = det3_list[0], det3_list[1], det3_list[2], det3_list[3], det3_list[4], det3_list[5], det3_list[6], det3_list[7], det3_list[8]
+
+    j = [a,b,c]
+    k = [d,e,f]
+    l = [g,h,i]
+    m = (a * ((e * i)-(f * h))) - (b * ((d * i)-(f * g))) + (e * g)
+
+    print(j)
+    print(k)
+    print(l)
+    return m
+
+
+#|A| = a(ei - fh) - b(di - fg) + c(dh - eg)
 
 #3×6 − 8×4 = 18 − 32 = −14
--- tests/test_mojave.py
+++ tests/test_mojave.py
@@ -18,3 +18,16 @@ class Det_2x2_Tests(unittest.TestCase):
         self.assertEqual(None, workingmojave.det2x2([2, 5, 2]))
         self.assertEqual(None, workingmojave.det2x2([2, 5,]))
         self.assertEqual(None, workingmojave.det2x2([2, ]))
+
+
+class Det_3x3_Tests(unittest.TestCase):
+    def testing_det3x3_valid(self):
+        self.assertEqual(44, workingmojave.det3x3([4, 2, 9, 2, 4, 4, 8, 4, 1]))
+        self.assertEqual(0, workingmojave.det3x3([0, 0, 0, 0, 0, 0, 0, 0,0]))
+        self.assertEqual(-24, workingmojave.det3x3([-2, -4, -7, -2, -3, -3, -6, -2, - 9]))
+
+
+    def testing_det3x3_valid(self):
+        self.assertEqual(None, workingmojave.det3x3(["barf", "tads", "shoes", "sonix", "fear", "beers", "wako", "loops", "shade"]))
+        self.assertEqual(None, workingmojave.det3x3([0, 0, 0, 0, 0, 0, 0,]))
+        self.assertEqual(None, workingmojave.det3x3([-2]))
