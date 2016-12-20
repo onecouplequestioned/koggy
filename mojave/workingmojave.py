d747f2e9bd86d2291ce714ce86d5f6be3deb8088
Hydrangea <onecouplequestioned.onecouplequestioned@gmail.com>
2016-12-20 03:38:52 -0500

    re-did some of the formatting for det_2x2

--- mojave/workingmojave.py
+++ mojave/workingmojave.py
@@ -15,27 +15,22 @@ def det2x2(det2_list):
     if len(det2_list) != 4:
         return None
 
-
-    a,b,c,d = det2_list[0], det2_list[1], det2_list[2], det2_list[3]
-
+    a,b,c,d = det2_list
     returnlist = [a,b,c,d]
 
-
     for i in returnlist:
-        if i == str(i):
+        if type(i) == str:
             return None
 
-
-    else:
-        g = ((a * b)-(c*d))
-        return g
+    g = ((a * b)-(c*d))
+    return g
 
 
 
 
 
 def det3x3(det3_list):
-    a,b,c,d,e,f,g,h,i = det3_list[0], det3_list[1], det3_list[2], det3_list[3], det3_list[4], det3_list[5], det3_list[6], det3_list[7], det3_list[8]
+    a,b,c,d,e,f,g,h,i = det3_list
 
     j = [a,b,c]
     k = [d,e,f]
